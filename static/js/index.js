6e8546d089cb581891f6526843232beb6f32eba8
JongHoon im <ijh0614@naver.com>
2021-12-01 22:27:27 +0900

    add index_logout.html - JongHoon

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,8 @@
 const form = document.querySelector(".js-form-in");
 const input = form.querySelector("input");
 
+//리스트 크기 10인거 3종류.
+
 function handleSubmit(event){
     event.preventDefault();
     const saveValue = input.value;
